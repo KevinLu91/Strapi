e01fdc60b34fdac2b83fc357218807fedf2f5b5a
kevin <kevinl_91@hotmail.com>
2021-01-10 19:43:57 +0100

    removed client

--- Client/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '87ae0cc5aa1fa5979f17547de06319c8'),
-    },
-  },
-});
--- Client/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '17e31781-8238-498c-a03e-160da053b8bc'
-};
